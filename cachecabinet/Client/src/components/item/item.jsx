c02dec6b6facc0e835ec9e783342690ab29ed7f6
sempercuriosus <vespers-garages.0n@icloud.com>
2024-01-04 19:34:51 -0500

    init on the signle item/structure

---
+++ cachecabinet/Client/src/components/item/item.jsx
@@ -0,0 +1,15 @@
+/**
+ * @component Item
+ * @description This is a single-item the user wishes to track in a collection
+ * @returns One Item's details
+ */
+function Item() {
+  return (
+    <div>
+      <p> Item Display Test </p>
+    </div>
+  );
+}
+
+export default Item;
+
