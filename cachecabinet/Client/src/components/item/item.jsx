cf27792391781d4bd1b9001963132b9a88ece3d9
sempercuriosus <vespers-garages.0n@icloud.com>
2024-01-04 19:47:48 -0500

    added some additional details to Item

--- cachecabinet/Client/src/components/item/item.jsx
+++ cachecabinet/Client/src/components/item/item.jsx
@@ -1,3 +1,4 @@
+import { Fragment } from 'react';
 /**
  * @component Item
  * @description This is a single-item the user wishes to track in a collection
@@ -5,9 +6,18 @@
  */
 function Item() {
   return (
-    <div>
-      <p> Item Display Test </p>
-    </div>
+    <Fragment>
+      <h2>Item Name</h2>
+      <h3>Item Description</h3>
+      <p>Item Date Added</p>
+      <p>Item For Sale</p>
+      <p>Item Quantity</p>
+      <img
+        src=''
+        alt='Preview of Item'>
+        [ Image Here ]
+      </img>
+    </Fragment>
   );
 }
 
