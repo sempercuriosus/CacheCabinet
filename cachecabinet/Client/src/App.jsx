1e53328ff87b987da2c8e607d76bb5f70c43fb35
keithprimas <136135449+keithprimas@users.noreply.github.com>
2024-01-04 19:59:47 -0500

    updated app.jsx

--- cachecabinet/Client/src/App.jsx
+++ cachecabinet/Client/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import './App.css'
 
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ApolloProvider client={client}>
+      <Router>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/collections" element={<Collections />} />
+            <Route path="/createcollection" element={<CreateCollection />} />
+            <Route path="/viewcollection" element={<Collection />} />
+            <Route path="/viewitems" element={<Item />} />
+            <Route path="/createitem" element={<CreateItem />} />
+          </Routes>
+      </Router >
+    </ApolloProvider>
+  );
 }
 
 export default App
