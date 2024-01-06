d46c67d1939a79cc0bf2a4d7c2ce7d2a8facddf5
sempercuriosus <vespers-garages.0n@icloud.com>
2024-01-06 15:07:22 -0500

    added the item assignment export

--- cachecabinet/Server/models/index.js
+++ cachecabinet/Server/models/index.js
@@ -1,4 +1,6 @@
 const Collection = require('./collection');
 const Item = require('./item');
+const ItemAssignment = require('./itemAssignment');
 
-module.exports = { Collection, Item };
+module.exports = { Collection, Item, ItemAssignment };
+
