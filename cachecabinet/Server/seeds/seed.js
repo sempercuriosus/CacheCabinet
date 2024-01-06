148f63e42c9074da444b09539c69252c7716c250
sempercuriosus <vespers-garages.0n@icloud.com>
2024-01-06 13:33:53 -0500

    updated logs, and got ready for User model

--- cachecabinet/Server/seeds/seed.js
+++ cachecabinet/Server/seeds/seed.js
@@ -1,18 +1,45 @@
 const db = require('../config/connection');
-const { Collection, Item } = require('../models');
+const { Collection, Item /*,User */ } = require('../models');
 const collectionSeeds = require('./collectionSeeds.json');
 const itemSeeds = require('./itemSeeds.json');
+const cleanDB = require('./cleanDB');
 
+// Connection Established
 db.once('open', async () => {
   try {
-    console.log('--- --- --- Start Process:', 'Write Seeds');
+    // Connection Note
+    //
+    console.info('The Connection is established.', 'Starting the process...');
 
+    // Cleanup the Test Data
+    //
+    console.log('>>--- Starting Process', '---> Clean Seeds');
+
+    await cleanDB('Collection', 'collections');
+    await cleanDB('Item', 'items');
+
+    console.log('>>--- Ending Process', '---> Clean Seeds');
+
+    // Seed the Database
+    //
+    console.log('>>--- Starting Process', '---> Write Seeds');
+
+    // Insert Collection Seed Data
+    //
     await Collection.insertMany(collectionSeeds);
 
+    // Insert Item Seed Data
+    //
     await Item.insertMany(itemSeeds);
 
-    console.log('--- --- --- End Process:', 'Write Seeds');
-    process.exit(0);
+    // Insert User Seed Data
+    //
+    // await User.insertMany(userSeeds);
+
+    console.log('>>--- Ending Process', '---> Write Seeds');
+
+    // Exit Gracefully
+    process.exit(1);
   } catch (error) {
     console.info(
       '--- ',
@@ -22,7 +49,7 @@ db.once('open', async () => {
 
     console.error(error);
 
-    // exiting the seed process on error
+    // Exit on Error
     process.exit(0);
   }
 });
