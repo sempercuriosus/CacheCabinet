bdd2af8c195d76652cee254f550d1399a7a1ba7a
NemiMoser <135074383+NemiMoser@users.noreply.github.com>
2024-01-08 19:32:09 -0500

    added Footer

---
+++ cachecabinet/Client/src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div>created by Eric Hulse, Keith Primas, Nohemi Moser, and Jonathan Montalvo</div>
+        </footer>
+    );
+};
+
+export default Footer;
