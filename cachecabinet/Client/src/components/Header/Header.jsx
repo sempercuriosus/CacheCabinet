b5079539e5cfc0169aa5503edf0b8f87c42be73e
NemiMoser <135074383+NemiMoser@users.noreply.github.com>
2024-01-08 19:32:27 -0500

    added Header

---
+++ cachecabinet/Client/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <main>
+            <a href="/" className="logo">CacheCabinet</a>
+            <nav>
+                <a href="">Log in</a>
+                <a href="">Register</a>
+                <a href="">Sign out</a>
+            </nav>
+        </main>
+    )
+};
+
+export default Header;
