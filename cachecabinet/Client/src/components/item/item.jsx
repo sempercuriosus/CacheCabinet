393a8113ba43bb244adb3dc11b9afe320343f163
sempercuriosus <vespers-garages.0n@icloud.com>
2024-01-08 21:06:08 -0500

    removed color pal.

--- cachecabinet/Client/src/components/item/item.jsx
+++ cachecabinet/Client/src/components/item/item.jsx
@@ -1,19 +1,10 @@
+import colorPalette from '../../utils/colorPalette';
+
 /**
  * @component Item
  * @description This is a single-item the user wishes to track in a collection
  * @returns One Item's details
  */
-
-const colorPalette = {
-  IVORY: '#E1DAD3',
-  NUDE: '#E4C9B6',
-  DUSTYROSE: '#D7A49A',
-  SAGE: '#B6B9A4',
-  BABYBLUE: '#A4B1BA',
-  BLACK: '#000',
-  GREY: '#606060',
-  WHITE: '#fff',
-};
 
 function Item() {
   return (
