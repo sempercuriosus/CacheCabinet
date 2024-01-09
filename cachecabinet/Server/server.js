42fcba47bd4ffcfd374e8b0a0bf46ecdce313793
JohnnyC7212 <jonathan.mont7212.com>
2024-01-08 21:06:40 -0500

    just server.js

--- cachecabinet/Server/server.js
+++ cachecabinet/Server/server.js
@@ -1,2 +1,47 @@
+const express = require('express');
+const path = require('path');
 
+
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+const { authMiddleware } = require('./utils/auth');
+
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+
+const app = express();
+
+const startApolloServer = async () => {
+    await server.start();
+
+    app.use(express.urlencoded({ extended: false }));
+    app.use(express.json());
+
+    app.use('/graphql', expressMiddleware(server, {
+        context: authMiddleware
+    }));
+
+    if ( process.env.NODE_ENV == 'production') {
+        app.use(express.static(path.join(__dirname, '../client/dist')));
+
+        app.get('*', (req, res) => {
+            res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+        });
+    }
+
+    db.once('open', () => {
+        app.listen(PORT, () => {
+            console.log(`API server runnning on port ${PORT}!`);
+            console.log(``);
+        });
+    });
+};
+
+startApolloServer();
   
