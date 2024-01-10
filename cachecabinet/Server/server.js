4963fd201f29c0c2d47f2985497166635ad0fbe4
sempercuriosus <vespers-garages.0n@icloud.com>
2024-01-09 19:10:22 -0500

    added server, route to check it is running, and commented out auth for now.

--- cachecabinet/Server/server.js
+++ cachecabinet/Server/server.js
@@ -1,2 +1,56 @@
+const express = require('express');
+const path = require('path');
 
-  
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+// const { authMiddleware } = require('./utils/auth');
+
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const app = express();
+
+const startApolloServer = async () => {
+  // start the server
+  await server.start();
+
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
+
+  // apply graphql for route handling
+  app.use(
+    '/graphql',
+    expressMiddleware(server, {
+      //   context: authMiddleware,
+    }),
+  );
+
+  app.get('/amiup', (req, res) => {
+    // Send the response with the specified HTML content
+    res.send('<h1>I am up!</h1><br/><h3>Thanks for checking on me.<h3/>');
+  });
+
+  if (process.env.NODE_ENV == 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  }
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server runnning on port ${PORT}!`);
+      console.log(``);
+    });
+  });
+};
+
+startApolloServer();
+
