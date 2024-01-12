d889b31805a86ff400bcbb9b6d95bb823c26bd69
sempercuriosus <vespers-garages.0n@icloud.com>
2024-01-11 19:54:53 -0500

    init on error page

---
+++ cachecabinet/Client/src/components/Error/ErrorPage.jsx
@@ -0,0 +1,16 @@
+// import Header from '../'
+// import Footer from '../';
+
+function ErrorPage() {
+  return (
+    <section className='content'>
+      <h1 className='title'>Route Was Not Found</h1>
+      <h3 className='title is-3'>
+        The Route that you have tried to access was not found. Please try again.
+      </h3>
+    </section>
+  );
+}
+
+export default ErrorPage;
+
