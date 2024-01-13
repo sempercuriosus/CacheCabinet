ebb2d9702b8e989d2bcd5a6dd5d60f88323c6878
sempercuriosus <vespers-garages.0n@icloud.com>
2024-01-13 14:42:46 -0500

    this was borked. removed what was there and reset.

--- cachecabinet/Client/vite.config.js
+++ cachecabinet/Client/vite.config.js
@@ -1,33 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-		//port: process.env.VITE_PORT || 3001,
-        open: true,
-		proxy: {
-			'^/graphql*': {
-				target: 'http://localhost:3001',
-				changeOrigin: true,
-				//rewrite: (path) => path.replace(/^\/api/, ''),
-			},
-			'^/api*': {
-				target: 'http://localhost:3001',
-				changeOrigin: true,
-				//rewrite: (path) => path.replace(/^\/api/, ''),
-			},
-			'^/api/v1*': {
-				target: 'http://localhost:3001',
-				changeOrigin: true,
-				//rewrite: (path) => path.replace(/^\/api\/v1/, ''),
-			},
-			'^/assets*': {
-				target: 'http://localhost:3001',
-				changeOrigin: true,
-				//rewrite: (path) => path.replace(/^\/api/, ''),
-			}
-		}
-  }
-})
+    port: 3000,
+    open: true,
+    proxy: {
+      '/graphql': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
+
