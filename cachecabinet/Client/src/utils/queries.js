aef5b452ef97891e22a5eb361a4bf9194d4ac7fb
sempercuriosus <vespers-garages.0n@icloud.com>
2024-01-14 11:08:42 -0500

    added queries

--- cachecabinet/Client/src/utils/queries.js
+++ cachecabinet/Client/src/utils/queries.js
@@ -1,8 +1,45 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_USER = gql`
-  query user($email: String!) {
-      _id
-      email
+//
+export const GET_USER_ASSIGNMENTS = gql`
+  query GetUserAssignments($userId: ID!) {
+    getUserAssignments(userId: $userId) {
+      userId
+      collections {
+        _id
+        name
+        description
+      }
+    }
   }
 `;
+
+//
+export const GET_COLLECTION = gql`
+  query GetCollection($collectionId: ID!) {
+    getCollection(collectionId: $collectionId) {
+      collectionId
+      items {
+        _id
+        name
+        description
+      }
+    }
+  }
+`;
+
+//
+export const GET_ITEM = gql`
+  query GetItem($itemId: ID!) {
+    getItem(itemId: $itemId) {
+      _id
+      name
+      description
+      quantity
+      purchasePrice
+      forSale
+      dateAdded
+    }
+  }
+`;
+
