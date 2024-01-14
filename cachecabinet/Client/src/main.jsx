3d23a4abdcec7002745a8dfc858bcf85b48ad4d1
sempercuriosus <vespers-garages.0n@icloud.com>
2024-01-14 13:39:27 -0500

    added error page back

--- cachecabinet/Client/src/main.jsx
+++ cachecabinet/Client/src/main.jsx
@@ -5,26 +5,26 @@ import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './pages/HomePage/Home.jsx';
 import CabinetPage from './pages/Main/CabinetPage.jsx';
-
-
+import ErrorPage from './components/Error/ErrorPage.jsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
-        element: <CabinetPage />
-      }, {
-        path: '/login',
-        element: <Home />
+        element: <CabinetPage />,
       },
-    ]
-  }
-])
-
+      {
+        path: '/login',
+        element: <Home />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
-)
+  <RouterProvider router={router} />,
+);
