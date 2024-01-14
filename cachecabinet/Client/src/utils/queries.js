4ccee0679ef629b81a9dad9d36f56a5877f11038
sempercuriosus <vespers-garages.0n@icloud.com>
2024-01-14 11:18:24 -0500

    described query

--- cachecabinet/Client/src/utils/queries.js
+++ cachecabinet/Client/src/utils/queries.js
@@ -1,6 +1,16 @@
 import { gql } from '@apollo/client';
 
-//
+/*
+ * GET USER ASSIGNMENTS
+ * gets all of a user's collections to display in cards
+ *
+ * needs
+ * user id
+ *
+ * returns
+ * collection _id, collection name, collection description
+ *
+ */
 export const GET_USER_ASSIGNMENTS = gql`
   query GetUserAssignments($userId: ID!) {
     getUserAssignments(userId: $userId) {
@@ -14,7 +24,19 @@ export const GET_USER_ASSIGNMENTS = gql`
   }
 `;
 
-//
+/*
+ * GET COLLECTION
+ * gets a single collection's item details
+ *
+ * needs
+ * collection id
+ *
+ * returns
+ * items id
+ * items name
+ * items description
+ *
+ */
 export const GET_COLLECTION = gql`
   query GetCollection($collectionId: ID!) {
     getCollection(collectionId: $collectionId) {
@@ -28,7 +50,21 @@ export const GET_COLLECTION = gql`
   }
 `;
 
-//
+/*
+ * GET ITEM
+ * gets a single item's details
+ *
+ * needs
+ * item id
+ *
+ * returns
+ * item name
+ * item description
+ * item quantity
+ * item purchasePrice
+ * item forSale
+ * item dateAdded
+ */
 export const GET_ITEM = gql`
   query GetItem($itemId: ID!) {
     getItem(itemId: $itemId) {
