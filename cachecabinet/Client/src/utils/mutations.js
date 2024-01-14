abbb0db283dd61eb57c85f3fe1dac829c92c4713
sempercuriosus <vespers-garages.0n@icloud.com>
2024-01-14 11:27:51 -0500

    described mutations

--- cachecabinet/Client/src/utils/mutations.js
+++ cachecabinet/Client/src/utils/mutations.js
@@ -1,7 +1,15 @@
 import { gql } from '@apollo/client';
 
-// LOGIN
-
+/*
+ * LOGIN
+ *
+ * needs
+ * email
+ * password
+ *
+ * returns
+ * token
+ */
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
@@ -13,8 +21,16 @@ export const LOGIN_USER = gql`
   }
 `;
 
-//  ADD A USER
-
+/*
+ * ADD A USER
+ *
+ * needs
+ * email
+ * password
+ *
+ * returns
+ * token
+ */
 export const ADD_USER = gql`
   mutation addUser($email: String!, $password: String!) {
     addUser(email: $email, password: $password) {
@@ -26,8 +42,20 @@ export const ADD_USER = gql`
   }
 `;
 
-//  ADD A COLLECTION
-
+/*
+ * ADD A COLLECTION
+ *
+ * needs
+ * user id
+ * new collection
+ * name
+ * description
+ *
+ * returns
+ * addCollection obj with
+ * name
+ * description
+ */
 export const ADD_COLLECTION = gql`
   mutation AddCollection($userId: ID!, $collectionData: NewCollection!) {
     addCollection(userId: $userId, collectionData: $collectionData) {
@@ -37,8 +65,22 @@ export const ADD_COLLECTION = gql`
   }
 `;
 
-//  ADD AN ITEM
-
+/*
+ * ADD AN ITEM
+ *
+ * needs
+ * user id
+ * collection id
+ *
+ * returns
+ * addItem obj with
+ *
+ * name
+ * description
+ * quantity
+ * purchasePrice
+ * forSale
+ */
 export const ADD_ITEM = gql`
   mutation AddItem($userId: ID!, $collectionId: ID!, $itemData: NewItem) {
     addItem(userId: $userId, collectionId: $collectionId, itemData: $itemData) {
@@ -51,8 +93,22 @@ export const ADD_ITEM = gql`
   }
 `;
 
-//  UPDATE A COLLECTION
-
+/*
+ * UPDATE A COLLECTION
+ *
+ * needs
+ * user id
+ * collection id
+ *
+ * collection data to update
+ * name
+ * description
+ *
+ * returns
+ * updated collection data
+ * name
+ * description
+ */
 export const UPDATE_COLLECTION = gql`
   mutation UpdateCollection(
     $userId: ID!
@@ -70,8 +126,28 @@ export const UPDATE_COLLECTION = gql`
   }
 `;
 
-//  UPDATE AN ITEM
-
+/*
+ * UPDATE AN ITEM
+ *
+ * needs
+ * user id
+ * item id
+ *
+ * item data to update
+ * name
+ * description
+ * quantity
+ * purchasePrice
+ * forSale
+ *
+ * returns
+ * updated item data
+ * name
+ * description
+ * quantity
+ * purchasePrice
+ * forSale
+ */
 export const UPDATE_ITEM = `mutation UpdateItem($userId: ID!, $itemId: ID!, $updatedItem: UpdateItem!) {
   updateItem(userId: $userId, itemId: $itemId, updatedItem: $updatedItem) {
     name
