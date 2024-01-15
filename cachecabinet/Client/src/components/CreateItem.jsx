b252b8daa491665010a498f9f03e80fa8bc62edf
sempercuriosus <vespers-garages.0n@icloud.com>
2024-01-15 15:58:22 -0500

    changed colors

--- cachecabinet/Client/src/components/CreateItem.jsx
+++ cachecabinet/Client/src/components/CreateItem.jsx
@@ -75,7 +75,7 @@ const CreateItem = ({ onAddItem }) => {
       <div className='card-content'>
         <button
           className='button is-primary'
-          style={{ backgroundColor: colorPalette.NUDE }}
+          style={{ backgroundColor: colorPalette.BABYBLUE }}
           type='button'
           onClick={handleImageUpload}>
           Upload Image
@@ -160,7 +160,7 @@ const CreateItem = ({ onAddItem }) => {
               <div className='control'>
                 <button
                   className='button is-primary'
-                  style={{ backgroundColor: colorPalette.NUDE }}
+                  style={{ backgroundColor: colorPalette.SAGE }}
                   type='button'
                   onClick={handleAddItem}>
                   Add Item
