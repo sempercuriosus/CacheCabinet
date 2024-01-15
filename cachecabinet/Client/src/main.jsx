42fc684e8722c9f12ae2d762c267207a2d2fd760
sempercuriosus <vespers-garages.0n@icloud.com>
2024-01-15 10:12:26 -0500

    added new routes

--- cachecabinet/Client/src/main.jsx
+++ cachecabinet/Client/src/main.jsx
@@ -6,6 +6,8 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './pages/HomePage/Home.jsx';
 import CabinetPage from './pages/Main/CabinetPage.jsx';
 import ErrorPage from './components/Error/ErrorPage.jsx';
+import ViewCollection from './components/ViewCollection.jsx';
+import Item from './components/item.jsx';
 
 const router = createBrowserRouter([
   {
@@ -21,6 +23,14 @@ const router = createBrowserRouter([
         path: '/main',
         element: <CabinetPage />,
       },
+      {
+        path: '/collection/:collectionId',
+        element: <ViewCollection />,
+      },
+      {
+        path: '/item/:itemId',
+        element: <Item />,
+      },
     ],
   },
 ]);
@@ -28,3 +38,4 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />,
 );
+
