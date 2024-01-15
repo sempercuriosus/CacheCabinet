4621f75a89025781447c2c9b650fdd332fddbc3d
sempercuriosus <vespers-garages.0n@icloud.com>
2024-01-15 14:00:26 -0500

    updated header

--- cachecabinet/Client/src/components/Header/Header.jsx
+++ cachecabinet/Client/src/components/Header/Header.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header className='level'>
+    <header className='level block'>
       <div className='title level-item'>
-        <div className='title'>🗃️ Cache Cabinet</div>
+        <div className='title'>🗃️ Cache Cabinet 🗑️</div>
       </div>
     </header>
   );
