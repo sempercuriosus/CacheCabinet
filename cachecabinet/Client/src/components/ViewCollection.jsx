3b7e1bbdd7b1a1d4cb125e5edb8b2ae445b6634b
sempercuriosus <vespers-garages.0n@icloud.com>
2024-01-15 19:16:09 -0500

    rm

--- cachecabinet/Client/src/components/ViewCollection.jsx
+++ cachecabinet/Client/src/components/ViewCollection.jsx
@@ -1,15 +1,11 @@
 import React, { useState, Fragment } from 'react';
 import { useParams } from 'react-router-dom';
 import Item from './item';
-import CreateItem from './CreateItem';
 import { useQuery } from '@apollo/client';
 import { GET_COLLECTION } from '../utils/queries';
 
 const ViewCollection = () => {
   const { collectionId } = useParams();
-
-  const [collection, setCollection] = useState([]);
-  const [showCreateItem, setShowCreateItem] = useState(true);
 
   const { loading, error, data } = useQuery(GET_COLLECTION, {
     variables: {
@@ -24,22 +20,6 @@ const ViewCollection = () => {
   if (loading) {
     return <h1 className='title is-3'>Loading...</h1>;
   }
-
-  const handleAddItem = (newItem) => {
-    // Update the collection state with the new item
-    setCollection((prevCollection) => [...prevCollection, newItem]);
-    // Hide the CreateItem component
-    setShowCreateItem(false);
-  };
-
-  const handleEditItem = (index, updatedItem) => {
-    // Update the collection state with the edited item
-    setCollection((prevCollection) => {
-      const newCollection = [...prevCollection];
-      newCollection[index] = updatedItem;
-      return newCollection;
-    });
-  };
 
   if (data) {
     const items = data.getCollection.items;
