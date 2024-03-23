5ba34c62a78b6d69147bf6ce52211742c5685c0e
Eric <vespers-garages.0n@icloud.com>
2024-03-23 17:03:51 -0400

    del the server, node modules, client from the /cachecabinet dir

--- Server/models/index.js
+++
@@ -1,7 +0,0 @@
-const Collection = require('./collection');
-const Item = require('./item');
-const ItemAssignment = require('./itemAssignment');
-const User = require('./User');
-
-module.exports = { Collection, Item, ItemAssignment, User };
-
