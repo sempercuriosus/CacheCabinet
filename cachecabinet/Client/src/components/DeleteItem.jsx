0e8c1ed3b3d50407def7053c529e915439eb55f8
Eric <vespers-garages.0n@icloud.com>
2024-03-24 11:27:16 -0400

    updated style for the delete item button and added modal for delete collection

--- cachecabinet/Client/src/components/DeleteItem.jsx
+++ cachecabinet/Client/src/components/DeleteItem.jsx
@@ -2,38 +2,43 @@ import React from 'react';
 import { useMutation } from '@apollo/client';
 import { DELETE_ITEM, GET_COLLECTION } from '../utils/queries';
 import { useNavigate } from 'react-router-dom';
-
+import colorPalette from '../utils/colorPalette';
 
 const DeleteItem = ({ itemId, collectionId, onClose }) => {
-    const navigate = useNavigate();
-    const [deleteItem] = useMutation(DELETE_ITEM, {
-        onCompleted: () => {
-            onClose();
-            navigate(`/collection/${collectionId}`);
+  const navigate = useNavigate();
+  const [deleteItem] = useMutation(DELETE_ITEM, {
+    onCompleted: () => {
+      onClose();
+      navigate(`/collection/${collectionId}`);
+    },
+    refetchQueries: [{ query: GET_COLLECTION, variables: { collectionId } }],
+  });
 
+  const handleDeleteItem = async () => {
+    try {
+      await deleteItem({
+        variables: {
+          itemId: itemId,
         },
-        refetchQueries: [{ query: GET_COLLECTION, variables: { collectionId } }],
-    });
+      });
+    } catch (error) {
+      console.error('Error deleting item:', error);
+    }
+  };
 
-    const handleDeleteItem = async () => {
-        try {
-            await deleteItem({
-                variables: {
-                    itemId: itemId,
-                },
-            });
-        } catch (error) {
-            console.error('Error deleting item:', error);
-        }
-    };
-
-    return (
-        <div>
-            <h2>Delete Item</h2>
-            <p>Are you sure you want to delete this item?</p>
-            <button onClick={handleDeleteItem}>Delete</button>
-        </div>
-    );
+  return (
+    <div>
+      <h2>Delete Item</h2>
+      <p>Are you sure you want to delete this item?</p>
+      <button
+        className='button'
+        style={{ backgroundColor: colorPalette.DUSTYROSE }}
+        onClick={handleDeleteItem}>
+        Delete
+      </button>
+    </div>
+  );
 };
 
 export default DeleteItem;
+
